7b0b96e934bc776ffc40e6ea9003d6b8a126aac0
Imaanvir Brar <imaanvir.brar@gov.ab.ca>
2022-10-03 03:17:55 -0600

    Fix bugs in Auth Function

--- services/auth/authorizer/index.ts
+++ services/auth/authorizer/index.ts
@@ -3,16 +3,23 @@ import {
     APIGatewaySimpleAuthorizerResult
   } from "aws-lambda";
   import jwt, { JwtPayload } from "jsonwebtoken";
-  import { Role, controllers } from './permissions'
+  import { Role, controllers, enumFromStringValue } from './permissions'
+
+  const publicKey = ['-----BEGIN PUBLIC KEY-----', 
+    'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAngFH+GEMWiduoA+QA03+Ov/ekWr5HaGxEacJ/AOoc2HEbNuXmVMVwOMtVHWlOpEXdLkHCWmLXHVLhATHiI/2HcLv6buB4AJnYz1ud6/+2of7y+9yfiC73JpJQgJV0g1eT76CxFPjZ2inlxR4H/erf/w0o8cGNW9B9TWXMzNy2Jpvak2t95Tl3mONIxEHSeJcftA8as4k8a73GB1E/FAy/4dkYGifOFP2/DH6Y1J2MgnfVBjZM/HV7i6DE58H+MaDTPPNZdWQN4qDCoBR/X8xCP8cVuBwmIPXwJ8cR2thIpdX5AOOlCkhUBUNv3iJgaQO9ynk0G9NH5cT3MROhFSL+wIDAQAB',
+    '-----END PUBLIC KEY-----'].join('\n');
   
   async function validateToken(token: string): Promise<string> {
     try {
-        var payload = await jwt.verify(token, 'secretKey', {
+      
+        var payload = await jwt.verify(token, publicKey, {
             issuer: 'https://access-uat.alberta.ca/auth/realms/5ce2889d-0965-4e85-a139-483662b2442c',
-            audience: ['trds:transcripts-api', 'account']
+            audience: ['trds:transcripts-api', 'account'],
+            algorithms: ['RS256'],
         });
+
         if (typeof(payload) === 'object')
-          payload = '334'
+          payload = JSON.stringify(payload);
         console.log(payload);
         return typeof(payload) === 'string' ? payload : JSON.stringify(payload);
       }
@@ -23,19 +30,23 @@ import {
 
   function extractRolesFromToken(tokenPayload: string): Role[] {
     const payload = JSON.parse(tokenPayload);
-    const rolesAsString: (keyof typeof Role)[] = payload?.resource_access?.['trds:transcripts-api']?.roles;
-    let roles: Role[]
+    const rolesAsString: string[] = payload?.resource_access?.['trds:transcripts-api']?.roles;
+    let roles: Role[] = [];
     rolesAsString.forEach(roleAsString => {
-        roles.push(Role[roleAsString]);
+        const role = enumFromStringValue(Role, roleAsString);
+        roles.push(role);
     })
     return roles;
   }
+
+
   
   export async function lambdaHandler(
     event: APIGatewayRequestAuthorizerEvent
   ): Promise<APIGatewaySimpleAuthorizerResult> {
     try {
-      const token = event?.headers?.["Authorization"];
+      // get Aiuthorization bearer token from headers and remove the 'Bearer' part
+      const token = event?.headers?.["Authorization"].replace('Bearer ', '');
   
       if (!token) {
         throw new Error("authorization token not found");
@@ -47,12 +58,11 @@ import {
       
       // perform authorization
       const userRoles = extractRolesFromToken(payload);
-      const authorizedRoles = controllers.find(x => x.path === event.path && x.method === event.httpMethod).Roles;
+      const authorizedRoles = controllers.find(x => x.path === event.path && x.method === event.httpMethod)?.Roles;
       const authorized = (userRoles.filter(userRole => authorizedRoles.includes(userRole))).length > 0;
 
       if (!authorized) {
         throw new Error("user is not authorized to perform this action");
-        
       }
 
       return {
--- services/auth/authorizer/permissions.ts
+++ services/auth/authorizer/permissions.ts
@@ -64,4 +64,10 @@ export const controllers: ApiController[] = [
     API_ORDERS_GET_CLIENT_DASHBOARD
 ]
 
+export function enumFromStringValue<T> (enm: { [s: string]: T}, value: string): T | undefined {
+    return (Object.values(enm) as unknown as string[]).includes(value)
+      ? value as unknown as T
+      : undefined;
+  }
+
 
